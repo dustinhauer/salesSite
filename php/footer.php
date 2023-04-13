e4b4fdcbf57b23801f0ac3b70c9b8e2a5b30bb8d
Dustin Hauer <dustinhauer@gmail.com>
2023-04-13 10:00:13 -0500

    Create footer.php

---
+++ php/footer.php
@@ -0,0 +1,13 @@
+			<footer id="footer">
+				<div class="container">
+					<ul class="icons">
+						<li><a href="http://www.twitter.com/mmelogistics" target=_blank class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="http://www.facebook.com/mmelogistics" target=_blank class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="http://www.instragram.com/mmelogistics" target=_blank class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="http://www.pinterest.com/mmelogistics" target=_blank class="icon fa-pinterest"><span class="label">Instagram</span></a></li>
+					</ul>
+				</div>
+				<div class="copyright">
+					&copy; MME Logistics. All rights reserved.
+				</div>
+			</footer>
