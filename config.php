4771a543b1a7ffc08bdcc8d0023eb70f7ed660e8
Dustin Hauer <dustinhauer@gmail.com>
2023-04-13 09:36:21 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,1 @@
-<?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'id10175828_dustinhauer');
-define('DB_PASSWORD', 'Midwest1918!');
-define('DB_NAME', 'id10175828_salespeople');
- 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
